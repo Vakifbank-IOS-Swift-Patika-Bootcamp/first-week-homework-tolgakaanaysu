3ef06df9d4e6e8010de486e388fb41c43e42bb01
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2022-10-31 12:41:56 +0300

    question 1

--- first-week-homework-tolgakaanaysu.playground/Contents.swift
+++ first-week-homework-tolgakaanaysu.playground/Contents.swift
@@ -1,2 +1,21 @@
 import UIKit
 
+//MARK: - Question 1
+func isPalindrome(str: String) -> Bool {
+    var strArray = Array(str)
+    var count = strArray.count
+    for i in 0..<(count / 2) {
+        if strArray[i] != strArray[count - 1 - i]{
+            print("\(str) palindrom değildir.")
+            return false
+        }
+    }
+    print("\(str) palindromdur.")
+    return true
+}
+
+isPalindrome(str: "Tolga")
+isPalindrome(str: "123321")
+isPalindrome(str: "Patika")
+isPalindrome(str: "12321")
+isPalindrome(str: "TolgaagloT")
